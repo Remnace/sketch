6dce7d895857fe87b7e30e9b4315d3cc8cb45183
Remnace <rileyae20@gmail.com>
2016-09-08 23:23:40 -0500

    Added random color function. Not implemented.

--- js/sketch.js
+++ js/sketch.js
@@ -1,24 +1,28 @@
-var size = 16;
-var boxSize = Math.floor(960/(size));
-//var $row = $('<div />', {class: 'row'}); Obsolete
+var size = 16; //Variable which stores the dimension of grid.
+var boxSize = Math.floor(960/(size)); //Stores size of each individual grid box.
 var $box = $('<div />', {class: 'box'});
 
+
+//Initial 
 $(document).ready(function() {
 	drawGrid(size);
 	draw();
 	clearGrid();
 });
 
+//Clears the grid back to default color.
 function clearGrid() {
 	$('.box').css('background', '#faebd7');
 }
 
+//Changes background color of a grid box when hovered over with mouse.
 function draw() {
 	$('div.box').hover(function() {
 		$(this).css('background', '#8ec252');
 	});
 }
 
+//Gets a new size for the grid and calls the drawGrid function with the new size.
 function newSize() {
 	var newSize = prompt('Enter a number between 0 and 65.');
 	if (newSize < 1 || newSize > 64) {
@@ -29,6 +33,7 @@ function newSize() {
 	}
 }
 
+//Draws the grid based on a given size.
 function drawGrid(sizeIn) {
 	var boxDivs = [];
 	size = sizeIn;
@@ -49,6 +54,7 @@ function drawGrid(sizeIn) {
 	gridLines();
 }
 
+//Checks if grid lines are enabled.
 function gridLines() {
 	var checked = $('#lines:checked').val();
 	if(checked){
@@ -57,4 +63,23 @@ function gridLines() {
 	else {
 		$('.box').css('border', 'none');
 	}
+}
+
+//Changes background color of a box to a random color.
+function drawRandom() {
+	$('div.box').hover(function() {
+		$(this).css('background', randomColor());
+	});
+}
+
+//Generates a random color
+function randomColor() {
+	var hexChars = '0123456789ABCDEF'.split('');
+	var color = '#';
+
+	for (var i = 0; i < 6; i++) {
+		color += hexChars[Math.floor(Math.random()*16)];
+	}
+
+	return color;
 }
